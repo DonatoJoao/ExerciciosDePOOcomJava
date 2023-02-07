d5b10101f2c137a354112bbb02b760e30f997759
DonatoJoao <joaod.morais@gmail.com>
2023-02-07 11:22:18 -0300

    Compreendendo Polimorfismo

---
+++ bytebank/src/bytebank/ControleBonificao.java
@@ -0,0 +1,15 @@
+package bytebank;
+
+public class ControleBonificao {
+
+	private double soma;
+	
+	public void registra (Funcionario funcionario) {
+		double bonificacao = funcionario.getBonificacao();
+		this.soma = this.soma + bonificacao;
+	}
+	
+	public double getSoma() {
+		return soma;
+	}
+}
---
+++ bytebank/src/bytebank/EditirVideo.java
@@ -0,0 +1,8 @@
+package bytebank;
+
+public class EditirVideo extends Funcionario {
+
+	public double getBonificacao() {
+		return super.getBonificacao() + 100;
+	}
+}
